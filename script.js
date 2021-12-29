4d35b1cf4248d355e86d63623b791a66630fec6b
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2021-12-29 16:01:19 -0300

    Requisito 10 - realizado

--- script.js
+++ script.js
@@ -16,15 +16,26 @@ function mostraTarefa() {
     tarefa.appendChild(textoTarefa)
     lista.appendChild(tarefa)
 }
+
 function corTarefa() {
     const tarefas = document.getElementById('lista-tarefas');
     tarefas.addEventListener('click', function (event) {
         let tarefa = document.querySelectorAll('li');
         for (let index = 0; index < tarefa.length; index += 1) {
             if (tarefa[index].style.backgroundColor = '#bde0fe') {
+            }
         }
-    }
         event.target.style.backgroundColor = 'rgb(128, 128, 128)';
     });
 }
 corTarefa()
+
+
+function limpar() {
+    const lista = document.querySelector('ol')
+    let limpar = document.getElementById("apaga-tudo")
+    limpar.addEventListener("click", function () {
+        lista.innerHTML = ''; //substitui o HTML pelo "vazio", ou seja, apaga a lista. 
+    })
+}
+limpar()
